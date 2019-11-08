da74dd426c6b15f90596435a4a50e0562a871cca
caribbeanyute <smithcleon@gmail.com>
2019-11-08 17:07:58 +0000

    Added fetch with alert

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ window.onload = function() {
     var anchors = document.getElementsByClassName("searchButton");
     for(var i = 0; i < anchors.length; i++) {
         var anchor = anchors[i];
-        url = "http://localhost:8080/superheroes.php"
+        url = "http://34.82.184.97:51568/superheroes.php";
         anchor.onclick = function() {
-            let response = await fetch(url);
-            if (response.ok) { // if HTTP-status is 200-299
-                // get the response body (the method explained below)
-                let json = await response.json();
-                alert(json);
-            } else {
-                alert("HTTP-Error: " + response.status);
-            }
+            fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                alert(data) // Prints result from `response.json()` in getRequest
+            })
+            .catch(error => console.error(error))
             
         }
     }
